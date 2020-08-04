bbaf66e941d0cef2e141052dff911d7da6b22d18
Joao Marcos Lopes Pinto <joaomarcoslp3@gmail.com>
2020-08-04 20:53:56 -0300

    Teacher Form page created

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+import './styles.css';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <Header title="Que incrível que você quer dar aulas.">
+        teste
+      </Header>
+    </div>
+  )
+}
+
+export default TeacherForm
